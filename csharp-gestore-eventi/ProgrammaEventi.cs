2c8a72fa60fb2ee5bbe049b8857245950cc16b37
KatiaLosignore <katialos.kl@gmail.com>
2023-09-18 14:51:11 +0200

    aggiunta classe ProgrammaEventi con costruttore

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgrammaEventi
+    {
+        // ATTRIBUTI
+        public string titolo;
+        public string Titolo
+        {
+            get
+            {
+                return this.titolo;
+
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException($"Il campo {titolo} non può essere vuoto!");
+                }
+                else
+                {
+                    this.titolo = value;
+                }
+            }
+        }
+
+        // COSTRUTTORE
+        public List<Evento> Eventi { get; set; }
+
+        public ProgrammaEventi(string titolo)
+        {
+            this.Titolo = titolo;
+            this.Eventi = new List<Evento>();
+        }
+
+
+    }
+
+
+}
+
+
+    
