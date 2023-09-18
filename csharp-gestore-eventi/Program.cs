d5373448234f9a975149ddc8ea08981c736bbb46
KatiaLosignore <katialos.kl@gmail.com>
2023-09-18 12:15:42 +0200

    aggiunta nuova istanza classe evento

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -10,7 +10,12 @@ Console.Write("Inserisci il titolo dell'evento: ");
 string titolo = Console.ReadLine();
 
 Console.Write("Inserisci la data dell'evento nel seguente formato (dd/MM/yyyy): ");
-string data = Console.ReadLine();
+if (!DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime data))
+{
+    Console.WriteLine("Data non valida. Il programma verrà chiuso.");
+    return;
+}
+
 
 Console.Write("Inserisci il numero dei posti totali: ");
 
@@ -22,10 +27,9 @@ while (!int.TryParse(Console.ReadLine(), out numeroPosti))
 
 // Istanzio la classe Evento
 
-Evento evento = new Evento(titolo, DateOnly.ParseExact(data, "dd/MM/yyyy"), numeroPosti);
+Evento evento = new Evento(titolo, data, numeroPosti);
 
-
-
+Console.WriteLine($"Hai creato un evento: {evento}");
 
 
 
